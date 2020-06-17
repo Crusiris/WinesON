769fe0f4caad7c3a853b2944ba6f2e46ba69861c
crusiris <cruzsirys@gmail.com>
2020-06-17 19:29:02 -0400

    ajustando variables de entorno

--- src/config/axiosMarket.js
+++ src/config/axiosMarket.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const clientAxiosMarket = axios.create({
-    baseURL: 'http://ec2-54-183-147-121.us-west-1.compute.amazonaws.com:8585',
+    baseURL: process.env.REACT_APP_URL_MARKET,
     headers: {
-        access_token: '22636ca690d932cc523065f4b3dea68ed3184bdb'
+        access_token: process.env.REACT_APP_TOKEN
     }
 });
 
--- src/config/axiosProduct.js
+++ src/config/axiosProduct.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const clientAxiosProduct = axios.create({
-    baseURL: 'http://ec2-54-183-147-121.us-west-1.compute.amazonaws.com:8383',
+    baseURL: process.env.REACT_APP_URL_PRODUCT,
     headers: {
-        access_token: '22636ca690d932cc523065f4b3dea68ed3184bdb'
+        access_token: process.env.REACT_APP_TOKEN
     }
 });
 
